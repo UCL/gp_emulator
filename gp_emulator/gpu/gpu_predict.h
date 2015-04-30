2d80a3e0bf4f23d402040b88f1006b58634c0f96
SinanShi <s.shi@ucl.ac.uk>
2015-04-30 14:48:44 +0100

    update header file

--- gp_emulator/gpu/gpu_predict.h
+++ gp_emulator/gpu/gpu_predict.h
@@ -2,17 +2,17 @@
 
 #include "Python.h"
 #include "arrayobject.h"
-//#include "C_arraytest.h"
 #include <math.h>
 #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// #include <time.h>
+#include <stdlib.h>
+ #include <string.h>
+ #include <time.h>
 /* Includes, cuda */
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <cublas_v2.h>
 
+#define real double
 
 /*
 * he
@@ -21,18 +21,43 @@
 //#define GPU_PREDICT_H
 
 
-void hi();
 //void hello();
 
 
-static PyObject *comp();
 
-static PyObject *predict(PyObject *self, PyObject *args);
 
 PyArrayObject *pyvector(PyObject *objin);
 double *pyvector_to_Carrayptrs(PyArrayObject *arrayin);
 double **pymatrix_to_Carrayptrs(PyArrayObject *arrayin);
 double **ptrvector(long n);
 
-//#endif
+PyObject *predict_wrap ( PyObject *self, PyObject *args );
+//void predict(real *c_theta_exp, real **c_inputs,real *c_invQt,real **c_invQ, real **c_testing, int N, int NN, int D, int theta_size);
+//extern "C" void predict(real *c_theta_exp, real *c_inputs,real *c_invQt,real *c_invQ, real *c_testing, int N, int NN, int D, int theta_size);
+__global__ void gpu_cdist(double *d_testing, int M, int N, int P);
 
+// error check macros
+#define cudaCheckErrors(msg) \
+    do { \
+        cudaError_t __err = cudaGetLastError(); \
+        if (__err != cudaSuccess) { \
+            fprintf(stderr, "Fatal error: %s (%s at %s:%d)\n", \
+                msg, cudaGetErrorString(__err), \
+                __FILE__, __LINE__); \
+            fprintf(stderr, "*** FAILED - ABORTING\n"); \
+            exit(1); \
+        } \
+    } while (0)
+
+// for CUBLAS V2 API
+#define cublasCheckErrors(fn) \
+    do { \
+        cublasStatus_t __err = fn; \
+        if (__err != CUBLAS_STATUS_SUCCESS) { \
+            fprintf(stderr, "Fatal cublas error: %d (at %s:%d)\n", \
+                (int)(__err), \
+                __FILE__, __LINE__); \
+            fprintf(stderr, "*** FAILED - ABORTING\n"); \
+            exit(1); \
+        } \
+    } while (0)
