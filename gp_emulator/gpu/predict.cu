1a3367961a0c4c991e55b08ee225a954dd3e7a95
SinanShi <s.shi@ucl.ac.uk>
2015-06-21 22:57:58 +0100

    better variable names predict.cu

--- gp_emulator/gpu/predict.cu
+++ gp_emulator/gpu/predict.cu
@@ -2,7 +2,7 @@
  * CUDA implementation of derived from GaussianProcess
  * in python code. 
  * 
- * Sinan Shi (UCL) 
+ * Sinan Shi 
  *********************************************/
 #include "gpu_predict.h"
 #include <stdlib.h>
@@ -13,13 +13,13 @@
  * GaussianPredict::predict
  *********************************************/
 extern "C"{
-void predict(const real *c_theta_exp, const real *c_inputs,const real *c_invQt,const real *c_invQ, const real *c_testing,  
-        real *c_mu, real *c_var, real *c_deriv,const int N,const int M, const int  D, const int theta_size)
+void predict(const real *c_theta_exp, const real *c_train,const real *c_invQt,
+        const real *c_invQ, const real *c_predict,  
+        real *c_result, real *c_error, real *c_deriv,
+        const int Npredict,const int Ntrain, const int  Ninputs, const int theta_size)
 {
     int i;
-//    cublasStatus_t stat;
     cublasHandle_t handle;
-    
     cublasCreate(&handle);
 
     real *c_theta_exp_sqrt;
@@ -31,41 +31,41 @@ void predict(const real *c_theta_exp, const real *c_inputs,const real *c_invQt,c
 
 
     //define device vector and matrices
-    real *d_inputs, *d_theta_exp, *d_theta_exp_sqrt, *d_invQt, *d_invQ, *d_testing;
+    real *d_train, *d_theta_exp, *d_theta_exp_sqrt, *d_invQt, *d_invQ, *d_predict;
 
     //allocate and copy vector on device 
     cudaMalloc( (void **)&d_theta_exp, sizeof(real) * theta_size );
     cudaMalloc( (void **)&d_theta_exp_sqrt, sizeof(real) * theta_size );
-    cudaMalloc( (void **)&d_invQt, sizeof(real) * M);
+    cudaMalloc( (void **)&d_invQt, sizeof(real) * Ntrain);
     cublasCheckErrors(cublasSetVector( theta_size, sizeof(real), c_theta_exp_sqrt, 1, d_theta_exp_sqrt, 1 ));
     cublasCheckErrors(cublasSetVector( theta_size, sizeof(real), c_theta_exp, 1, d_theta_exp, 1 ));
-    cublasCheckErrors(cublasSetVector( M, sizeof(real), c_invQt, 1, d_invQt, 1));
+    cublasCheckErrors(cublasSetVector( Ntrain, sizeof(real), c_invQt, 1, d_invQt, 1));
 
     //allocate and copy matrix on device
-    cudaMalloc( (void **)&d_inputs, sizeof(real) * M * D );
-    cudaMalloc( (void **)&d_invQ, sizeof(real) * M * M );
-    cudaMalloc( (void **)&d_testing, sizeof(real) * N * D );
-    cublasCheckErrors(cublasSetMatrix( M, D, sizeof(real), c_inputs, M, d_inputs, M ));
-    cublasCheckErrors(cublasSetMatrix( M, M, sizeof(real), c_invQ, M, d_invQ, M ));
-    cublasCheckErrors(cublasSetMatrix( N, D, sizeof(real), c_testing, N, d_testing, N));
+    cudaMalloc( (void **)&d_train, sizeof(real) * Ntrain * Ninputs );
+    cudaMalloc( (void **)&d_invQ, sizeof(real) * Ntrain * Ntrain );
+    cudaMalloc( (void **)&d_predict, sizeof(real) * Npredict * Ninputs );
+    cublasCheckErrors(cublasSetMatrix( Ntrain, Ninputs, sizeof(real), c_train, Ntrain, d_train, Ntrain ));
+    cublasCheckErrors(cublasSetMatrix( Ntrain, Ntrain, sizeof(real), c_invQ, Ntrain, d_invQ, Ntrain ));
+    cublasCheckErrors(cublasSetMatrix( Npredict, Ninputs, sizeof(real), c_predict, Npredict, d_predict, Npredict));
     
 
         
     /*********************************
      * Euclidian distance calculation
-     * a = cidist(sqrt(expX_{,D}) * inputs * sqrt(expX_{,D} * testing)))
-     * Notice: a is equivalent to a^T in python due to column major fashion
+     * a = cidist(sqrt(expX_{,Ninputs}) * inputs * sqrt(expX_{,Ninputs} * testing)))
+     * Npredictotice: a is equivalent to a^T in python due to column major fashion
      ********************************/ 
     real *d_res_temp1, *d_res_temp2, *d_a;
-    cudaMalloc((void **)&d_res_temp1, sizeof(real) * M * D);
-    cudaMalloc((void **)&d_res_temp2, sizeof(real) * N * D);
-    cudaMalloc((void **)&d_a, sizeof(real) * M * N);
+    cudaMalloc((void **)&d_res_temp1, sizeof(real) * Ntrain * Ninputs);
+    cudaMalloc((void **)&d_res_temp2, sizeof(real) * Npredict * Ninputs);
+    cudaMalloc((void **)&d_a, sizeof(real) * Ntrain * Npredict);
 
-    gpu_vectorTimesMatrix(d_inputs, d_theta_exp_sqrt, d_res_temp1, M, D);
-    gpu_vectorTimesMatrix(d_testing, d_theta_exp_sqrt, d_res_temp2, N, D);
-    gpu_init_array( d_a, 0.0, N * M );
-    gpu_cdist(d_res_temp1, d_res_temp2, d_a, M, D, N, D);
-    gpu_matrixExp(d_a, -0.5, c_theta_exp[D], M * N);
+    gpu_vectorTimesMatrix(d_train, d_theta_exp_sqrt, d_res_temp1, Ntrain, Ninputs);
+    gpu_vectorTimesMatrix(d_predict, d_theta_exp_sqrt, d_res_temp2, Npredict, Ninputs);
+    gpu_init_array( d_a, 0.0, Npredict * Ntrain );
+    gpu_cdist(d_res_temp1, d_res_temp2, d_a, Ntrain, Ninputs, Npredict, Ninputs);
+    gpu_matrixExp(d_a, -0.5, c_theta_exp[Ninputs], Ntrain * Npredict);
 
     cudaFree(d_res_temp1);
     cudaFree(d_res_temp2);
@@ -74,12 +74,12 @@ void predict(const real *c_theta_exp, const real *c_inputs,const real *c_invQt,c
      * compute mu:
      * mu = a * invQt (dot product)
      ********************************/
-    real *d_mu;
-    cudaMalloc((void **)&d_mu, sizeof(real) * N);
+    real *d_result;
+    cudaMalloc((void **)&d_result, sizeof(real) * Npredict);
     real alpha = 1.f;
     real beta = 0.f;
-    /*cublasCheckErrors(*/CUBLAS_GEMV(handle, CUBLAS_OP_N, N, M, &alpha, d_a, N, d_invQt, 1, &beta, d_mu, 1);//);
-    cudaMemcpy(c_mu, d_mu, sizeof(real) * N, cudaMemcpyDeviceToHost);
+    /*cublasCheckErrors(*/CUBLAS_GEMV(handle, CUBLAS_OP_N, Npredict, Ntrain, &alpha, d_a, Npredict, d_invQt, 1, &beta, d_result, 1);//);
+    cudaMemcpy(c_result, d_result, sizeof(real) * Npredict, cudaMemcpyDeviceToHost);
        
     
    /*********************************
@@ -87,23 +87,23 @@ void predict(const real *c_theta_exp, const real *c_inputs,const real *c_invQt,c
     * var = b - rowsum(a * dot(invQ, a_T))
     ********************************/
     real *d_temp_dot, *d_a_T;
-    real *d_var;
+    real *d_error;
 
-    cudaMalloc((void **)&d_temp_dot, sizeof(real) * M * N);
-    cudaMalloc((void **)&d_a_T, sizeof(real) * M * N);
+    cudaMalloc((void **)&d_temp_dot, sizeof(real) * Ntrain * Npredict);
+    cudaMalloc((void **)&d_a_T, sizeof(real) * Ntrain * Npredict);
 
-    cublasCheckErrors(CUBLAS_GEMM(handle, CUBLAS_OP_N, CUBLAS_OP_T, M, N, M, &alpha, d_invQ, M, d_a, N, &beta, d_temp_dot, M));
-    cublasCheckErrors(CUBLAS_GEAM(handle, CUBLAS_OP_T, CUBLAS_OP_N, M, N, &alpha, d_a, N, &beta, d_a, M, d_a_T, M));
+    cublasCheckErrors(CUBLAS_GEMM(handle, CUBLAS_OP_N, CUBLAS_OP_T, Ntrain, Npredict, Ntrain, &alpha, d_invQ, Ntrain, d_a, Npredict, &beta, d_temp_dot, Ntrain));
+    cublasCheckErrors(CUBLAS_GEAM(handle, CUBLAS_OP_T, CUBLAS_OP_N, Ntrain, Npredict, &alpha, d_a, Npredict, &beta, d_a, Ntrain, d_a_T, Ntrain));
     
     cudaFree(d_a);
     cudaFree(d_invQ);
     
-    gpu_elementwiseMult(d_a_T, d_temp_dot, M * N);
-    d_var = gpu_rowSum(d_temp_dot, M, N);
-    gpu_scalarMinusVec(d_var, c_theta_exp[D], N );
-    cudaMemcpy(c_var, d_var, sizeof(real) * N, cudaMemcpyDeviceToHost);
+    gpu_elementwiseMult(d_a_T, d_temp_dot, Ntrain * Npredict);
+    d_error = gpu_rowSum(d_temp_dot, Ntrain, Npredict);
+    gpu_scalarMinusVec(d_error, c_theta_exp[Ninputs], Npredict );
+    cudaMemcpy(c_error, d_error, sizeof(real) * Npredict, cudaMemcpyDeviceToHost);
 
-    cudaFree(d_var);
+    cudaFree(d_error);
     cudaFree(d_temp_dot);
 
 
@@ -112,35 +112,35 @@ void predict(const real *c_theta_exp, const real *c_inputs,const real *c_invQt,c
     * 
     ********************************/
     real *d_deriv;
-    cudaMalloc((void **)&d_deriv, sizeof(real) * N );
+    cudaMalloc((void **)&d_deriv, sizeof(real) * Npredict );
    
     real *d_aa;
-    cudaMalloc((void **)&d_aa, sizeof(real) * M * N);
+    cudaMalloc((void **)&d_aa, sizeof(real) * Ntrain * Npredict);
     real *ptr_inputs, *ptr_testing, *ptr_deriv;
-    ptr_inputs = d_inputs;
-    ptr_testing = d_testing;
+    ptr_inputs = d_train;
+    ptr_testing = d_predict;
     ptr_deriv = c_deriv;
 
-     for( i = 0; i < D; ++i){
-        gpu_crossMinus(ptr_inputs, ptr_testing, d_aa, M, N );
-        ptr_inputs = ptr_inputs + M;
-        ptr_testing = ptr_testing + N;
+     for( i = 0; i < Ninputs; ++i){
+        gpu_crossMinus(ptr_inputs, ptr_testing, d_aa, Ntrain, Npredict );
+        ptr_inputs = ptr_inputs + Ntrain;
+        ptr_testing = ptr_testing + Npredict;
         alpha = c_theta_exp[i];
-        gpu_elementwiseMult(d_a_T, d_aa, M * N);
-        cublasCheckErrors(CUBLAS_GEMV(handle, CUBLAS_OP_T, M, N, &alpha, d_aa, M, d_invQt, 1, &beta, d_deriv,1));
+        gpu_elementwiseMult(d_a_T, d_aa, Ntrain * Npredict);
+        cublasCheckErrors(CUBLAS_GEMV(handle, CUBLAS_OP_T, Ntrain, Npredict, &alpha, d_aa, Ntrain, d_invQt, 1, &beta, d_deriv,1));
 
-        cudaMemcpy(ptr_deriv, d_deriv, sizeof(real) * N, cudaMemcpyDeviceToHost);
-        ptr_deriv = ptr_deriv + N;
+        cudaMemcpy(ptr_deriv, d_deriv, sizeof(real) * Npredict, cudaMemcpyDeviceToHost);
+        ptr_deriv = ptr_deriv + Npredict;
      }
 
-     cudaFree(d_mu);
+     cudaFree(d_result);
      cudaFree(d_invQt);
      cudaFree(d_a_T);
      cudaFree(d_aa);
-     cudaFree(d_inputs);
+     cudaFree(d_train);
      cudaFree(d_deriv);
      cudaFree(d_theta_exp);
-     cudaFree(d_testing);
+     cudaFree(d_predict);
    
 }
 }
