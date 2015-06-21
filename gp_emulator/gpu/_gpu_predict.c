7b5baca6c645347a5db6c5d84435a0f6112fe1cf
SinanShi <s.shi@ucl.ac.uk>
2015-06-21 22:36:26 +0100

    change variable name

--- gp_emulator/gpu/_gpu_predict.c
+++ gp_emulator/gpu/_gpu_predict.c
@@ -1,7 +1,15 @@
+/**********************************//**
+ * source code of share library _gpu_predict.so
+ * importing inputs from python
+ * calling GPU predict function by predict_wrap()
+ * python utility functions are based on the following template:
+ *  http://wiki.scipy.org/Cookbook/C_Extensions/NumPy_arrays
+ **********************************/
+
 #include "gpu_predict.h"
 #include<errno.h>
 
-/*********************************
+/*********************************//**
  * Set up the methods table
  *********************************/
 static PyMethodDef gpuMethods[] = 
@@ -10,7 +18,7 @@ static PyMethodDef gpuMethods[] =
     {NULL, NULL}     /* Sentinel - marks the end of this structure */
 };
 
-/**********************************
+/**********************************//**
  * Initialize the C_test functions 
  * Module name must be _C_arraytest in compile and linked 
  **********************************/
@@ -21,7 +29,7 @@ void init_gpu_predict()
 }
 
 
-/**********************************
+/**********************************//**
  * Check the dimension and data type 
  * of input python arrays.
  **********************************/
@@ -48,7 +56,7 @@ void checkRealVector(PyArrayObject *vec)
 
 
 
-/**********************************
+/**********************************//**
  * Convert data (vector) from PyArrayObject to real
  **********************************/
 real *pyvector_to_Carrayptrs(PyArrayObject *arrayin)  
@@ -59,43 +67,43 @@ real *pyvector_to_Carrayptrs(PyArrayObject *arrayin)
 
 
 
-/**********************************
+/**********************************//**
  * getPredictDataFromPython
- * Assign multiple input python array to C array. 
- * Both of them are in 1D. 
+ * Assign multiple input python arrays, which are all in 1D
+ * to C array.
  **********************************/
 void getPredictDataFromPython(PyObject *args, real **c_theta_exp, real **c_invQt, real **c_invQ, 
-                              real **c_testing, real **c_inputs,
-                              real **c_mu, real **c_var, real **c_deriv,
-                              int *N, int *M, int *D, int *theta_size)
+                              real **c_predict, real **c_train,
+                              real **c_result, real **c_error, real **c_deriv,
+                              int *Npredict, int *Ntrain, int *Ninputs, int *theta_size)
 {
-    PyArrayObject *py_theta_exp, *py_inputs, *py_invQt, *py_invQ, *py_testing;
-    PyArrayObject *py_mu, *py_var, *py_deriv;
+    PyArrayObject *py_theta_exp, *py_train, *py_invQt, *py_invQ, *py_predict;
+    PyArrayObject *py_result, *py_error, *py_deriv;
 
     PyArg_ParseTuple ( args, "O!O!O!O!O!O!O!O!iiii",
         &PyArray_Type, &py_theta_exp,
-        &PyArray_Type, &py_inputs,
+        &PyArray_Type, &py_train,
         &PyArray_Type, &py_invQt,
         &PyArray_Type, &py_invQ,
-        &PyArray_Type, &py_testing,
-        &PyArray_Type, &py_mu,
-        &PyArray_Type, &py_var,
+        &PyArray_Type, &py_predict,
+        &PyArray_Type, &py_result,
+        &PyArray_Type, &py_error,
         &PyArray_Type, &py_deriv,
-        N, M, D, theta_size);
+        Npredict, Ntrain, Ninputs, theta_size);
 
     *c_theta_exp = pyvector_to_Carrayptrs( py_theta_exp);
-    *c_inputs = pyvector_to_Carrayptrs( py_inputs );
+    *c_train = pyvector_to_Carrayptrs( py_train );
     *c_invQt = pyvector_to_Carrayptrs( py_invQt );
     *c_invQ = pyvector_to_Carrayptrs( py_invQ );
-    *c_testing = pyvector_to_Carrayptrs( py_testing );
-    *c_mu = pyvector_to_Carrayptrs( py_mu );
-    *c_var = pyvector_to_Carrayptrs( py_var );
+    *c_predict = pyvector_to_Carrayptrs( py_predict );
+    *c_result = pyvector_to_Carrayptrs( py_result );
+    *c_error = pyvector_to_Carrayptrs( py_error );
     *c_deriv = pyvector_to_Carrayptrs( py_deriv );
 }
 
 
 
-/**********************************
+/**********************************//**
  * predict_wrap
  * 1) call getPredictDataFromPython;
  * 2) transpose 2D arrays (data arranged in 1D) to column major;
@@ -103,35 +111,32 @@ void getPredictDataFromPython(PyObject *args, real **c_theta_exp, real **c_invQt
  **********************************/
 PyObject *predict_wrap ( PyObject *self, PyObject *args )
 {
-    int N,M,D,theta_size;
+    int Npredict, Ntrain, Ninputs, theta_size;
     real *c_theta_exp;
-    real *c_inputs, *c_invQt, *c_invQ, *c_testing;
-    real *c_mu, *c_var, *c_deriv;
+    real *c_train, *c_invQt, *c_invQ, *c_predict;
+    real *c_result, *c_error, *c_deriv;
    
     getPredictDataFromPython(args, &c_theta_exp, &c_invQt, &c_invQ,
-                             &c_testing, &c_inputs,
-                             &c_mu, &c_var, &c_deriv,
-                             &N, &M, &D, &theta_size);
-
+                             &c_predict, &c_train,
+                             &c_result, &c_error, &c_deriv,
+                             &Npredict, &Ntrain, &Ninputs,  &theta_size);
 
     //transpose 2D array to column major to cope with cublas
-    real *c_invQ_T, *c_inputs_T, *c_testing_T;
-    c_invQ_T = computeTranspose( c_invQ, M, M );
-    c_inputs_T = computeTranspose( c_inputs, D, M );
-    c_testing_T = computeTranspose( c_testing, D, N);
+    real *c_invQ_T, *c_train_T, *c_predict_T;
+    c_invQ_T = computeTranspose( c_invQ, Ntrain, Ntrain );
+    c_train_T = computeTranspose( c_train, Ninputs, Ntrain );
+    c_predict_T = computeTranspose( c_predict, Ninputs, Npredict);
 
-    predict( c_theta_exp, c_inputs_T, c_invQt, c_invQ_T, c_testing_T, 
-            c_mu, c_var, c_deriv,
-            N, M, D, theta_size );
-
+    predict( c_theta_exp, c_train_T, c_invQt, c_invQ_T, c_predict_T, 
+            c_result, c_error, c_deriv,
+            Npredict, Ntrain, Ninputs, theta_size );
 
     free(c_invQ_T);
-    free(c_inputs_T);
-    free(c_testing_T);
+    free(c_train_T);
+    free(c_predict_T);
    
-    return Py_BuildValue("");
-
-    
+    Py_INCREF(Py_None);
+    return Py_None;
 }
 
 
