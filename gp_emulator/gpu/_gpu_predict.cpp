05858b4c433fef99c9f0bdfd81fa1c30ef6b77ab
SinanShi <s.shi@ucl.ac.uk>
2015-06-24 09:58:39 +0100

    c++ implementation of predict function

---
+++ gp_emulator/gpu/_gpu_predict.cpp
@@ -0,0 +1,189 @@
+/**********************************//**
+ * source code of share library _gpu_predict.so
+ * importing inputs from python
+ * calling GPU predict function by predict_wrap()
+ * python utility functions are based on the following template:
+ *  http://wiki.scipy.org/Cookbook/C_Extensions/NumPy_arrays
+ **********************************/
+
+#include "gpu_predict.h"
+#include<errno.h>
+
+/*********************************//**
+ * Set up the methods table
+ *********************************/
+static PyMethodDef gpuMethods[] = 
+{
+    {"predict_wrap", predict_wrap, METH_VARARGS},
+    {NULL, NULL}     /* Sentinel - marks the end of this structure */
+};
+
+/**********************************//**
+ * Initialize the C_test functions 
+ * Module name must be _C_arraytest in compile and linked 
+ **********************************/
+
+extern "C"{
+void init_gpu_predict()  
+{
+    (void) Py_InitModule("_gpu_predict", gpuMethods);
+    import_array();  // Must be present for NumPy.  Called first after above line.
+}
+}
+
+
+
+/**********************************//**
+ * Check the dimension and data type 
+ * of input python arrays.
+ **********************************/
+void checkRealVector(PyArrayObject *vec)  
+{
+    if( sizeof( real ) == sizeof( double ) )
+    {
+        if (vec->descr->type_num != NPY_DOUBLE || vec->nd != 1)  
+        {
+            printf( "In checkRealVector: array must be of type Double vector\n");
+            exit(EXIT_FAILURE);
+        }
+       
+    }
+    if( sizeof( real ) == sizeof( float ) ) 
+    {
+        if (vec->descr->type_num != NPY_FLOAT32 || vec->nd != 1)  
+        {
+            printf("In checkRealVector: array must be of type Float32 vector\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+
+
+/**********************************//**
+ * Convert data (vector) from PyArrayObject to real
+ **********************************/
+real *pyvector_to_Carrayptrs(PyArrayObject *arrayin)  
+{
+    checkRealVector(arrayin);
+    return  (real *) arrayin->data;  /* pointer to arrayin data as real */
+}
+
+
+
+/**********************************//**
+ * getPredictDataFromPython
+ * Assign multiple input python arrays, which are all in 1D
+ * to C array.
+ **********************************/
+void getPredictDataFromPython(PyObject *args, real **c_theta_exp, real **c_invQt, real **c_invQ, 
+                              real **c_predict, real **c_train,
+                              real **c_result, real **c_error, real **c_deriv,
+                              int *Npredict, int *Ntrain, int *Ninputs, int *theta_size)
+{
+    PyArrayObject *py_theta_exp, *py_train, *py_invQt, *py_invQ, *py_predict;
+    PyArrayObject *py_result, *py_error, *py_deriv;
+
+    PyArg_ParseTuple ( args, "O!O!O!O!O!O!O!O!iiii",
+        &PyArray_Type, &py_theta_exp,
+        &PyArray_Type, &py_train,
+        &PyArray_Type, &py_invQt,
+        &PyArray_Type, &py_invQ,
+        &PyArray_Type, &py_predict,
+        &PyArray_Type, &py_result,
+        &PyArray_Type, &py_error,
+        &PyArray_Type, &py_deriv,
+        Npredict, Ntrain, Ninputs, theta_size);
+
+    *c_theta_exp = pyvector_to_Carrayptrs( py_theta_exp);
+    *c_train = pyvector_to_Carrayptrs( py_train );
+    *c_invQt = pyvector_to_Carrayptrs( py_invQt );
+    *c_invQ = pyvector_to_Carrayptrs( py_invQ );
+    *c_predict = pyvector_to_Carrayptrs( py_predict );
+    *c_result = pyvector_to_Carrayptrs( py_result );
+    *c_error = pyvector_to_Carrayptrs( py_error );
+    *c_deriv = pyvector_to_Carrayptrs( py_deriv );
+}
+
+
+
+/**********************************//**
+ * predict_wrap
+ * 1) call getPredictDataFromPython;
+ * 2) transpose 2D arrays (data arranged in 1D) to column major;
+ * 3) calling GPU predict function;
+ **********************************/
+PyObject *predict_wrap ( PyObject *self, PyObject *args )
+{
+    int i;
+    int Npredict, Ntrain, Ninputs, theta_size;
+    real *c_theta_exp;
+    real *c_train, *c_invQt, *c_invQ, *c_predict;
+    real *c_result, *c_error, *c_deriv;
+   
+    getPredictDataFromPython(args, &c_theta_exp, &c_invQt, &c_invQ,
+                             &c_predict, &c_train,
+                             &c_result, &c_error, &c_deriv,
+                             &Npredict, &Ntrain, &Ninputs,  &theta_size);
+
+    //transpose 2D array to column major to cope with cublas
+    real *c_invQ_T, *c_train_T, *c_predict_T;
+    c_invQ_T = computeTranspose( c_invQ, Ntrain, Ntrain );
+    c_train_T = computeTranspose( c_train, Ninputs, Ntrain );
+    c_predict_T = computeTranspose( c_predict, Ninputs, Npredict);
+
+    
+    real *c_theta_exp_sqrt;
+    c_theta_exp_sqrt = (real *)malloc( sizeof(real) * theta_size );
+    for( i=0; i < theta_size; i++ )
+    {
+        c_theta_exp_sqrt[i] = sqrt( c_theta_exp[i] );
+    }
+
+
+    // predict( c_theta_exp, c_train_T, c_invQt, c_invQ_T, c_predict_T, 
+    //         c_result, c_error, c_deriv,
+    //         Npredict, Ntrain, Ninputs, theta_size );
+
+    gpuPredict gpu_predict(
+        c_theta_exp, c_theta_exp_sqrt,
+        c_invQt, c_invQ_T, 
+        c_predict_T,c_train_T, 
+        c_result, c_error, c_deriv,
+        Npredict, Ntrain, Ninputs, theta_size );
+    gpu_predict.predict();
+
+
+
+
+
+
+    free(c_invQ_T);
+    free(c_train_T);
+    free(c_predict_T);
+   
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ gp_emulator/gpu/predictcpp.cu
@@ -0,0 +1,184 @@
+
+/*********************************************//** 
+ * CUDA implementation of derived from GaussianProcess
+ * in python code. 
+ * 
+ * Sinan Shi 
+ *********************************************/
+#include "gpu_predict.h"
+#include <stdlib.h>
+#define debug
+
+
+void gpuPredict::init_gpu(void)
+{
+    cublasCreate(&handle);
+    cudaMalloc( (void **)&d_theta_exp, sizeof(real) * theta_size );
+    cudaMalloc( (void **)&d_theta_exp_sqrt, sizeof(real) * theta_size );
+    cudaMalloc( (void **)&d_invQt, sizeof(real) * Ntrain);
+    cublasCheckErrors(cublasSetVector( 
+                theta_size, sizeof(real), c_theta_exp_sqrt, 1, d_theta_exp_sqrt, 1 ));
+    cublasCheckErrors(cublasSetVector( 
+                theta_size, sizeof(real), c_theta_exp, 1, d_theta_exp, 1 ));
+    cublasCheckErrors(cublasSetVector( 
+                Ntrain, sizeof(real), c_invQt, 1, d_invQt, 1));
+
+    //allocate and copy matrix on device
+    cudaMalloc( (void **)&d_train, sizeof(real) * Ntrain * Ninputs );
+    cudaMalloc( (void **)&d_invQ, sizeof(real) * Ntrain * Ntrain );
+    cudaMalloc( (void **)&d_predict, sizeof(real) * Npredict * Ninputs );
+    cublasCheckErrors(cublasSetMatrix( 
+                Ntrain, Ninputs, sizeof(real), c_train, Ntrain, d_train, Ntrain ));
+    cublasCheckErrors(cublasSetMatrix( 
+                Ntrain, Ntrain, sizeof(real), c_invQ, Ntrain, d_invQ, Ntrain ));
+    cublasCheckErrors(cublasSetMatrix( 
+                Npredict, Ninputs, sizeof(real), c_predict, Npredict, d_predict, Npredict));
+
+}
+
+/*********************************//*
+ * Euclidian distance calculation
+ * 1) res_mv1 = theta_exp_sqrt_{,Ninputs} * train
+ * 2) res_mv2 = theta_exp_sqrt_{,Ninputs} * predict
+ * 3) dist_matrix = cidist(res_mv1, res_mv2)
+ * 4) dist_matrix = -0.5 * exp(expX_{Ninputs})
+ * Notice: distance is equivalent to distance^T (a^T) in python
+ ********************************/ 
+void gpuPredict::compute_distance(void)
+{
+    real *d_res_mv1, *d_res_mv2;
+    cudaMalloc((void **)&d_res_mv1, sizeof(real) * Ntrain * Ninputs);
+    cudaMalloc((void **)&d_res_mv2, sizeof(real) * Npredict * Ninputs);
+    cudaMalloc((void **)&d_dist_matrix, sizeof(real) * Ntrain * Npredict);
+
+    gpu_vectorTimesMatrix(d_train, d_theta_exp_sqrt, d_res_mv1, Ntrain, Ninputs);
+    gpu_vectorTimesMatrix(d_predict, d_theta_exp_sqrt, d_res_mv2, Npredict, Ninputs);
+    gpu_init_array( d_dist_matrix, 0.0, Npredict * Ntrain );
+    gpu_cdist(d_res_mv1, d_res_mv2, d_dist_matrix, Ntrain, Ninputs, Npredict, Ninputs);
+    gpu_matrixExp(d_dist_matrix, -0.5, c_theta_exp[Ninputs], Ntrain * Npredict);
+
+    cudaFree(d_res_mv1);
+    cudaFree(d_res_mv2);
+}
+
+/*********************************//*
+ * compute result:
+ * c_result = dist_matrix * invQt (dot product)
+ ********************************/
+void gpuPredict::compute_result(void)
+{
+    real *d_result;
+    cudaMalloc((void **)&d_result, sizeof(real) * Npredict);
+    real alpha = 1.f;
+    real beta = 0.f;
+    cublasCheckErrors(CUBLAS_GEMV(handle, CUBLAS_OP_N, Npredict, Ntrain, &alpha, d_dist_matrix, 
+                Npredict, d_invQt, 1, &beta, d_result, 1));
+    cudaMemcpy(c_result, d_result, sizeof(real) * Npredict, cudaMemcpyDeviceToHost);
+    cudaFree(d_result);
+}
+
+
+real * gpuPredict::gpu_transpose(real *d_matrix, const int nrow, const int ncol)
+{
+    real *d_matrix_T;
+    real alpha = 1.f;
+    real beta = 0.f;
+    cudaMalloc((void **)&d_matrix_T, sizeof(real) * nrow * ncol );
+    cublasCheckErrors(CUBLAS_GEAM(handle, CUBLAS_OP_T, CUBLAS_OP_N, 
+                nrow, ncol, &alpha, d_matrix, ncol, &beta, 
+                d_matrix, nrow, d_matrix_T, nrow));
+    return( d_matrix_T );
+}
+
+/*********************************
+ * compute error:
+ * c_error = b - rowsum(a * dot(invQ, d_dist_matrix_T))
+ * arguments d_invQ, d_dist_matrix have been freed.
+ ********************************/
+void gpuPredict::compute_error()
+{
+    real alpha = 1.f;
+    real beta = 0.f;
+    real *d_res_dot;
+    real *d_error;
+    
+    cudaMalloc((void **)&d_res_dot, sizeof(real) * Ntrain * Npredict);
+    cublasCheckErrors(CUBLAS_GEMM(
+                handle, CUBLAS_OP_N, CUBLAS_OP_T, 
+                Ntrain, Npredict, Ntrain, 
+                &alpha, d_invQ, Ntrain, 
+                d_dist_matrix, Npredict, 
+                &beta, d_res_dot, Ntrain));  // dot(invQ, d_dist_matrix_T)
+    d_dist_matrix_T = gpuPredict::gpu_transpose(d_dist_matrix, Ntrain, Npredict);
+    gpu_elementwiseMult(d_dist_matrix_T, d_res_dot, Ntrain * Npredict);
+    d_error = gpu_rowSum(d_res_dot, Ntrain, Npredict);
+    gpu_scalarMinusVec(d_error, c_theta_exp[Ninputs], Npredict );
+ 
+    cudaMemcpy(c_error, d_error, sizeof(real) * Npredict, cudaMemcpyDeviceToHost);
+
+    cudaFree(d_dist_matrix);
+    cudaFree(d_invQ);
+    cudaFree(d_error);
+    cudaFree(d_res_dot);
+}
+
+
+
+/*********************************//*
+ * compute deriv:
+ *  
+ ********************************/
+void gpuPredict::compute_deriv( void )
+{
+    int i;
+    real alpha;
+    real beta = 0.f;
+ 
+    real *d_deriv, *d_aa;
+    cudaMalloc((void **)&d_deriv, sizeof(real) * Npredict );
+    cudaMalloc((void **)&d_aa, sizeof(real) * Ntrain * Npredict);
+    real *ptr_train, *ptr_predict, *ptr_deriv;
+    ptr_train = d_train;
+    ptr_predict = d_predict;
+    ptr_deriv = c_deriv;
+
+     for( i = 0; i < Ninputs; ++i){
+        gpu_crossMinus(ptr_train, ptr_predict, d_aa, Ntrain, Npredict );
+        ptr_train = ptr_train + Ntrain;
+        ptr_predict = ptr_predict + Npredict;
+        alpha = c_theta_exp[i];
+        gpu_elementwiseMult(d_dist_matrix_T, d_aa, Ntrain * Npredict);
+        cublasCheckErrors(CUBLAS_GEMV(handle, CUBLAS_OP_T, Ntrain, Npredict, &alpha, d_aa, Ntrain, d_invQt, 1, &beta, d_deriv,1));
+
+        cudaMemcpy(ptr_deriv, d_deriv, sizeof(real) * Npredict, cudaMemcpyDeviceToHost);
+        ptr_deriv = ptr_deriv + Npredict;
+     }
+
+     cudaFree(d_deriv);
+     cudaFree(d_aa);
+}
+
+
+void gpuPredict::predict( void )
+{
+     gpuPredict::init_gpu();
+     gpuPredict::compute_distance();
+     gpuPredict::compute_result();
+     gpuPredict::compute_error();
+     gpuPredict::compute_deriv();
+    
+    cublasDestroy(handle);
+    cudaFree(d_invQt);
+    cudaFree(d_dist_matrix_T);
+    cudaFree(d_train);
+    cudaFree(d_theta_exp);
+    cudaFree(d_predict);
+    cudaFree(d_theta_exp_sqrt);
+}
+
+
+
+
+
+
+
