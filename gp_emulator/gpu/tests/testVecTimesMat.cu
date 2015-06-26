c32e28cde3f08f1ac53b566c0a0e4e64c94815eb
SinanShi <s.shi@ucl.ac.uk>
2015-06-26 10:41:07 +0100

    typo

--- gp_emulator/gpu/tests/testVecTimesMat.cu
+++ gp_emulator/gpu/tests/testVecTimesMat.cu
@@ -1,6 +1,5 @@
 #include "gpu_predict_test.h"
 
-
 void testVecTimesMat(const real *c_vec,const  real *c_matrix, const real *c_res,const int vec_len, const int matrix_nrows, const int matrix_ncols)
 {
     real *d_vec, *d_matrix, *d_res;
@@ -18,9 +17,9 @@ void testVecTimesMat(const real *c_vec,const  real *c_matrix, const real *c_res,
     c_matrix_T = computeTranspose(c_matrix, matrix_ncols, matrix_nrows);
 
     cublasCheckErrors(cublasSetVector( vec_len, sizeof(real), c_vec, 1, d_vec, 1 ));
-    cublasCheckErrors(cublasSetmatrixrix( matrix_nrows, matrix_ncols, sizeof(real), c_matrix_T, matrix_nrows, d_matrix, matrix_nrows));
+    cublasCheckErrors(cublasSetMatrix( matrix_nrows, matrix_ncols, sizeof(real), c_matrix_T, matrix_nrows, d_matrix, matrix_nrows));
     
-    gpu_vectorTimesmatrixrix( d_matrix, d_vec, d_res, matrix_nrows, matrix_ncols);
+    gpu_vectorTimesMatrix( d_matrix, d_vec, d_res, matrix_nrows, matrix_ncols);
 
     cudaMemcpy(c_res_gpu, d_res, sizeof(real) * matrix_nrows * matrix_ncols, cudaMemcpyDeviceToHost);
     c_res_gpu = computeTranspose(c_res_gpu, matrix_nrows, matrix_ncols);
