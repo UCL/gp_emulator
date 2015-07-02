70434f3802a71bc05d96260bde87b2fac164c294
SinanShi <s.shi@ucl.ac.uk>
2015-07-02 13:50:53 +0100

    full timing function of pure_c

--- gp_emulator/gpu/pure_c.cpp
+++ gp_emulator/gpu/pure_c.cpp
@@ -49,23 +49,35 @@ void pureCPredict::compute_result(void)
     c_res_mv1 = (real *)malloc(sizeof(real) * Ntrain * Ninputs);
     c_res_mv2 = (real *)malloc(sizeof(real) * Npredict * Ninputs);
     c_dist_matrix = (real *)malloc(sizeof(real) * Npredict * Ntrain);
+
+    start = clock();
     cpu_vectorTimesMatrix(c_train, c_theta_exp_sqrt, c_res_mv1, Ntrain, Ninputs);
     cpu_vectorTimesMatrix(c_predict, c_theta_exp_sqrt, c_res_mv2, Npredict, Ninputs);
-start = clock();
-    cpu_cdist(c_res_mv1, c_res_mv2, c_dist_matrix, Ntrain, Ninputs, Npredict, Ninputs);
-diff = clock() - start;
-int msec = diff * 1000 / CLOCKS_PER_SEC;
-printf("cdist:  %f\n",float(msec)/1000);
+    diff = clock() -start;
+    printf("mvel %f\n", (float)diff/CLOCKS_PER_SEC);
 
+    
+    start = clock();
+    cpu_cdist(c_res_mv1, c_res_mv2, c_dist_matrix, Ntrain, Ninputs, Npredict, Ninputs);
+    diff = clock() - start;
+    int msec = diff * 1000 / CLOCKS_PER_SEC;
+    printf("cdist:  %f\n",float(msec)/1000);
+
+
+    start = clock();
     for( i = 0; i < Ntrain * Npredict; ++i)
         c_dist_matrix[i] = c_theta_exp[Ninputs] * exp( -0.5 * c_dist_matrix[i]);
+    diff = clock() - start;
+    printf("exp-ele %f\n",float(diff)/CLOCKS_PER_SEC);
+
+    
+    start = clock();
     BLAS_GEMV(CblasColMajor, CblasNoTrans, Npredict, Ntrain, 1.0, c_dist_matrix, Npredict, c_invQt, 1, 0.0, c_result, 1);
+    diff = clock() - start;
+    printf("mv1  %f\n",float(diff)/CLOCKS_PER_SEC);
     free(c_res_mv1);
     free(c_res_mv2);
 }
-
-
-
 
 
 void pureCPredict::compute_error(void)
@@ -74,23 +86,36 @@ void pureCPredict::compute_error(void)
     real *result_dot, *vec_one;
     result_dot = (real *)malloc( sizeof(real) * Ntrain * Npredict);
     vec_one = (real *)malloc( sizeof(real) * Npredict);
+    clock_t start, end;
+
+    start = clock();
     BLAS_GEMM(CblasColMajor, CblasNoTrans, CblasTrans, Ntrain, Npredict, Ntrain, 1.0, 
             c_invQ, Ntrain, c_dist_matrix, Npredict, 0.0, result_dot, Ntrain);
+    end = clock();
+    printf("mm %f\n",(float)(end-start)/CLOCKS_PER_SEC);
+    
     c_dist_matrix_T = computeTranspose(c_dist_matrix, Npredict, Ntrain);
+
+    start = clock();
     for( i = 0; i < Npredict * Ntrain; ++i )
         result_dot[i] = c_dist_matrix_T[i] * result_dot[i];
+    end = clock();
+    printf("mul-ele  %f\n",(float)(end-start)/CLOCKS_PER_SEC);
+
+    start = clock();
     for( i = 0; i < Npredict; ++i)
     {
         c_error[i] = 0.0;
         vec_one[i] = 1.0;
     }
-
     BLAS_GEMV(CblasColMajor, CblasTrans, Ntrain, Npredict, 
             1.0, result_dot, Ntrain, vec_one, 1, 0.0, c_error, 1);
-
     for( i = 0; i < Npredict; ++i){
         c_error[i] = c_theta_exp[Ninputs] - c_error[i];
     }
+    end = clock();
+    printf("sum  %f\n",(float)(end-start)/CLOCKS_PER_SEC);
+
     free(c_dist_matrix);
     free(result_dot);
     free(vec_one);
@@ -101,8 +126,8 @@ void pureCPredict::compute_error(void)
 void pureCPredict::compute_deriv(void)
 {
     int i, j, n;
-    clock_t start, end, diff;
-    diff=0;
+    clock_t start, end, diff, diff_cm, diff_mv;
+    diff=0; diff_cm=0; diff_mv=0;
     real *aa;
     real *ptr_train, *ptr_predict, *ptr_deriv;
     ptr_train = c_train;
@@ -110,17 +135,21 @@ void pureCPredict::compute_deriv(void)
     ptr_deriv = c_deriv;
     aa = (real *)malloc( sizeof(real) * Ntrain * Npredict );
     real alpha;
+
     for( n = 0; n < Ninputs; ++n)
     {
-        for( i = 0; i < Ntrain; ++i )
+        start = clock();
+        for( j = 0; j < Npredict; ++j )
         {
-            for( j = 0; j < Npredict; ++j )
+            for( i = 0; i < Ntrain; ++i )
             {
                 aa[IDX2D(i, j, Ntrain)] = ptr_train[i] - ptr_predict[j];
             }
         }
         ptr_train = ptr_train + Ntrain;
         ptr_predict = ptr_predict + Npredict;
+        end = clock();
+        diff_cm +=end - start;
 
         start = clock();
         for(i = 0; i < Ntrain * Npredict; ++i )
@@ -131,11 +160,17 @@ void pureCPredict::compute_deriv(void)
         diff += end - start;
 
         alpha = c_theta_exp[n];
+        start = clock();
         BLAS_GEMV(CblasColMajor, CblasTrans, Ntrain, Npredict, alpha, aa, Ntrain, c_invQt, 1, 0, ptr_deriv, 1);
+        end = clock();
+        diff_mv += end - start;
+
         ptr_deriv = ptr_deriv + Npredict;
     }
+    printf("cross_minus %f\n", (float)diff_cm/CLOCKS_PER_SEC);
     printf("mat_ele2 %f\n", (float)(diff/CLOCKS_PER_SEC));
-    
+    printf("mv2 %f\n", (float)diff_mv/CLOCKS_PER_SEC);
+
    free(aa);
 }
 
